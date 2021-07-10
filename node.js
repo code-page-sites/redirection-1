dc60a7e6b77bbf6d3933a4865adfc1aebd95843d
Kayden Lee <kayden@programmer.net>
2021-07-10 19:37:32 +0800

    Update node.js

--- node.js
+++ node.js
@@ -3,3 +3,7 @@ cloudinary.config({
   api_key: '389325557239844', 
   api_secret: 'e-hx0d2hLynhy2wXmcCqEulg-KI' 
 });
+
+cloudinary.v2.uploader.upload("https://replace.vercel.app/download.png",
+  { public_id: "download" }, 
+  function(error, result) {console.log(result); });
