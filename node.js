1e4334320657b3ce12eb3eb98cfd2dd007d9d0fd
Kayden Lee <kayden@programmer.net>
2021-08-24 17:54:16 +0800

    Delete node.js

--- node.js
+++
@@ -1,9 +0,0 @@
-cloudinary.config({ 
-  cloud_name: 'kayden', 
-  api_key: '389325557239844', 
-  api_secret: 'e-hx0d2hLynhy2wXmcCqEulg-KI' 
-});
-
-cloudinary.v2.uploader.upload("https://replace.vercel.app/download.png",
-  { public_id: "download" }, 
-  function(error, result) {console.log(result); });
